f76694ef26ad00009185f74b4639075bd54b382e
linhtran <ltran1@brynmawr.edu>
2021-07-12 17:34:36 +0700

    set up node and pathfindingvisualizer folders

---
+++ path-finding-visualizer/src/PathFindingVisualizer/PathFindingVisualizer.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import Node from './Node/Node'
+import './PathFindingVisualizer.css'
+
+export default class PathFindingVisualizer extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <div>
+                abc
+                <Node></Node>
+            </div>
+        )
+    }
+}
