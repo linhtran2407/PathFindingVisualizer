1a6e1a3bcf522f3b31ef995f9115261cc56287fe
linhtran <ltran1@brynmawr.edu>
2021-07-15 01:05:11 +0700

    finish draft

--- path-finding-visualizer/src/PathFindingVisualizer/PathFindingVisualizer.jsx
+++ path-finding-visualizer/src/PathFindingVisualizer/PathFindingVisualizer.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Node from './Node/Node'
 import './PathFindingVisualizer.css'
+import { dijkstra, getNodesInShortestPathOrder } from '../algorithms/dijkstra';
 
 const START_NODE_ROW = 10;
 const START_NODE_COL = 15;
@@ -12,7 +13,7 @@ export default class PathFindingVisualizer extends Component {
         super(props)
         this.state = {
             grid: [], // arrays of nodes for cells in grid
-            mouseIsPressed: false,
+            mouseIsPressed: false, // when press the mouse and drag, used when create wall
         }
     }
 
@@ -21,40 +22,97 @@ export default class PathFindingVisualizer extends Component {
         this.setState({grid})
     }
 
+    // mouse down is when we press, but not release, the mouse
+    // we want the node to be toggled into wall 
+    handleMouseDown(row, col){
+        const newGrid = getNewGridWithWallToggled(this.state.grid, row, col)
+        this.setState({grid: newGrid, mouseIsPressed: true})
+    }
 
-    // handleMouseDown(){
-    //     const newGrid = getNewGridWithWallToggled(this.state.grid, row, col);
-    //     this.setState({grid: newGrid, mouseIsPressed: true});
-    // }
+    // enter is when we pressed the mouse already and are hovering
+    // through the grid
+    handleMouseEnter(row, col){
+        // we dont want to toggle every cell into wall when we are hovering 
+        // but not pressing the mouse
+        if (!this.state.mouseIsPressed) {return}
 
-    // handleMouseUp(){
+        const newGrid = getNewGridWithWallToggled(this.state.grid, row, col)
+        this.setState({grid: newGrid})
+    }
 
-    // }
+    handleMouseUp(){
+        this.setState({mouseIsPressed: false})
+    }
+
+
+    animateDijkstra(visitedNodesInOrder, nodesInShortestPathOrder) {
+        for (let i=0; i<visitedNodesInOrder.length; i++){
+            // if reach the finish node, animate the shortest path
+            if (i === visitedNodesInOrder.length){
+                setTimeout(() => {
+                    this.animateShortestPath(nodesInShortestPathOrder)
+                }, 10*i);
+                return
+            }
+            // meanwhile, just show the path
+            setTimeout(() => {
+                const node = visitedNodesInOrder[i]
+                document.getElementById(`node-${node.row}-${node.col}`).className =
+                'node node-visited';
+            }, 10*i);
+        }
+    }
+
+    animateShortestPath(nodesInShortestPathOrder) {
+        for (let i=0; i<nodesInShortestPathOrder.length; i++){
+        setTimeout(() => {
+            const node = nodesInShortestPathOrder[i]
+            document.getElementById(`node-${node.row}-${node.col}`).className =
+          'node node-shortest-path';
+        }, 50 * i);
+        }
+    }
+
+    visualizeDijkstra() {
+        const {grid} = this.state
+        const startNode = grid[START_NODE_ROW][START_NODE_COL]
+        const finishNode = grid[FINISH_NODE_ROW][FINISH_NODE_COL]
+        const visitedNodesInOrder = dijkstra(grid, startNode, finishNode)
+        const nodesInShortestPathOrder = getNodesInShortestPathOrder(finishNode)
+        this.animateDijkstra(visitedNodesInOrder, nodesInShortestPathOrder)
+    }
 
     render() {
         const {grid, mouseIsPressed} = this.state;
-        console.log(grid)
 
         return (
+            <>
+            <button onClick={() => this.visualizeDijkstra()}>
+            Visualize Dijkstra's Algorithm
+            </button>
             <div className="grid">
                 {grid.map((row, rowIdx) => {
                     return (
                     <div key={rowIdx}>
                         {row.map((node, nodeIdx) => {
                             const {row, col, isFinish, isStart, isWall} = node;
-                            return <Node
+                            return (<Node
                             key={nodeIdx}
                             col={col}
                             isStart={isStart}
                             isFinish={isFinish}
                             isWall={isWall}
+                            onMouseDown = {(row, col) => this.handleMouseDown(row, col)}
+                            onMouseEnter = {(row, col) => this.handleMouseEnter(row, col)}
+                            onMouseUp = {(row, col) => this.handleMouseUp(row, col)}
                             mouseIsPressed={mouseIsPressed}
-                            row={row}></Node>
+                            row={row}></Node>)
                         })}
                     </div>
                     )
                 })}
             </div>
+            </>
         )
     }
 }
@@ -84,3 +142,17 @@ const createNode = (col, row) => {
       previousNode: null,
     };
 };
+
+
+// toggle the grid into a wall
+const getNewGridWithWallToggled = (grid, row, col) => {
+    const newGrid = grid.slice()
+    const node = newGrid[row][col]
+    const newNode = {
+        ...node,
+        isWall: !node.isWall
+    }
+
+    newGrid[row][col] = newNode
+    return newGrid
+}
--- path-finding-visualizer/src/algorithms/dijkstra.js
+++ path-finding-visualizer/src/algorithms/dijkstra.js
@@ -4,18 +4,22 @@
 // compute the shortest path by backtracking from the finish node.
 
 export function dijkstra(grid, startNode, finishNode) {
-    const dist = []
     startNode.distance = 0
     const visitedNodesInOrder = []
     const unvisitedNodes = getAllNodes(grid)
 
     while(!!unvisitedNodes.length){
         // get the min (closest node) and re-heapify to maintain min-heap structure
-        const closestNode = dequeue(unvisitedNodes)
+        sort(unvisitedNodes)
+        const closestNode = unvisitedNodes.shift()
 
         // skip if meet the wall
         if (closestNode.isWall) continue
-        
+
+        // If the closest node is at a distance of infinity,
+        // we must be trapped and should therefore stop.
+        if (closestNode.distance === Infinity) return visitedNodesInOrder;
+
         // mark as visited and push into the array
         closestNode.isVisited = true
         visitedNodesInOrder.push(closestNode)
@@ -29,6 +33,8 @@ export function dijkstra(grid, startNode, finishNode) {
         // neighbors node by 1
         updateUnvisitedNeighbors(closestNode, grid)
     }
+
+    return visitedNodesInOrder
 }
 
 function getUnvisitedNeighbors(node, grid) {
@@ -37,7 +43,7 @@ function getUnvisitedNeighbors(node, grid) {
     // check for validity of each coordinate
     if (row>0) {neighbors.push(grid[row-1][col])}
     if (col>0) {neighbors.push(grid[row][col-1])}
-    if (row<grid[0].length-1) {neighbors.push(grid[row+1][col])}
+    if (row<grid.length-1) {neighbors.push(grid[row+1][col])}
     if (col<grid[0].length-1) {neighbors.push(grid[row][col+1])}
     return neighbors.filter(nei => !nei.isVisited)
 }
@@ -54,85 +60,103 @@ function getAllNodes(grid){
     const nodes = []
     for (const row of grid){
         for (const node of row){
-            enqueue(nodes, node)
+            nodes.push(node)
         }
     }
     return nodes
 }
 
-function dequeue(unvisitedNodes){
-    if (unvisitedNodes.length === 0) {return null}
-    // store and return the min of the heap
-    const min = unvisitedNodes[0]
-
-    /** re-heapify */ 
-    // pop the last val and set as the new node
-    const end = unvisitedNodes.shift()
-    if (unvisitedNodes.length > 0){
-        unvisitedNodes[0] = end
-
-        let index = 0
-        const length = unvisitedNodes.length
-        const element = unvisitedNodes[index]
-
-        while(true){
-            let leftIndex = index*2 + 1
-            let rightIndex = index*2 + 2
-            let rightChild, leftChild
-            let swap = null
-
-            // check if left child is valid
-            if (leftIndex < length){
-                // swap if satisfied
-                leftChild = unvisitedNodes[leftIndex]
-                if(element.distance > leftChild.distance){
-                    swap = leftIndex
-                }
-            }
-
-            // check if right child is valid
-            if (rightIndex < length){
-                rightChild = unvisitedNodes[rightIndex]
-                // if no swap made and right child is less prioritized than the current node
-                // or there is a swap but the right is less prioritized than the left
-                if (swap === null && element.distance > rightChild.distance || swap !== null && rightChild.distance < leftChild.distance){
-                    swap = rightIndex
-                }
-            }
-
-            // stopping condition: when there is no swap could be made
-            if (swap === null) break
-            // else swap
-            unvisitedNodes[index] = unvisitedNodes[swap]
-            unvisitedNodes[swap] = element
-
-            // update index to continue checking
-            index = swap
-        }
-    }
-
-    return min
+function sort(unvisitedNodes) {
+    unvisitedNodes.sort((a,b) => a.distance - b.distance)
 }
 
-function enqueue(nodes, node) {
-    nodes.push(node)
+// function dequeue(unvisitedNodes){
+//     if (unvisitedNodes.length === 0) {return null}
+//     // store and return the min of the heap
+//     const min = unvisitedNodes[0]
 
-    // store the new node's index and value
-    const index = nodes.length-1
-    const element = nodes[index]
+//     /** re-heapify */ 
+//     // pop the last val and set as the new node
+//     const end = unvisitedNodes.shift()
+//     if (unvisitedNodes.length > 0){
+//         unvisitedNodes[0] = end
 
-    while(index>0){
-        // store the new node's parent's index and value
-        const parentIndex = Math.floor((index+1)/2)
-        const parent = nodes[parentIndex]
+//         let index = 0
+//         const length = unvisitedNodes.length
+//         const element = unvisitedNodes[index]
 
-        // compare distance then heapify
-        // if found the correct place, break
-        if (element.distance >= parent.distance){break}
-        // else swap parent and new node
-        nodes[index] = parent
-        nodes[parentIndex] = element
-        // update the index to continue heapifying
-        index = parentIndex
+//         while(true){
+//             let leftIndex = index*2 + 1
+//             let rightIndex = index*2 + 2
+//             let rightChild, leftChild
+//             let swap = null
+
+//             // check if left child is valid
+//             if (leftIndex < length){
+//                 // swap if satisfied
+//                 leftChild = unvisitedNodes[leftIndex]
+//                 if(element.distance > leftChild.distance){
+//                     swap = leftIndex
+//                 }
+//             }
+
+//             // check if right child is valid
+//             if (rightIndex < length){
+//                 rightChild = unvisitedNodes[rightIndex]
+//                 // if no swap made and right child is less prioritized than the current node
+//                 // or there is a swap but the right is less prioritized than the left
+//                 if ((swap === null && element.distance > rightChild.distance) || 
+//                     (swap !== null && rightChild.distance < leftChild.distance)){
+//                     swap = rightIndex
+//                 }
+//             }
+
+//             // stopping condition: when there is no swap could be made
+//             if (swap === null) break
+//             // else swap
+//             unvisitedNodes[index] = unvisitedNodes[swap]
+//             unvisitedNodes[swap] = element
+
+//             // update index to continue checking
+//             index = swap
+//         }
+//     }
+
+//     return min
+// }
+
+// function enqueue(nodes, node) {
+//     nodes.push(node)
+
+//     // store the new node's index and value
+//     let index = nodes.length-1
+//     const element = nodes[index]
+
+//     while(index>0){
+//         // store the new node's parent's index and value
+//         const parentIndex = Math.floor((index+1)/2)
+//         const parent = nodes[parentIndex]
+
+//         // compare distance then heapify
+//         // if found the correct place, break
+//         if (element.distance >= parent.distance){break}
+//         // else swap parent and new node
+//         nodes[index] = parent
+//         nodes[parentIndex] = element
+//         // update the index to continue heapifying
+//         index = parentIndex
+//     }
+// }
+
+// Backtracks from the finishNode to find the shortest path.
+// Only works when called *after* the dijkstra method above.
+export function getNodesInShortestPathOrder(finishNode) {
+    const nodesInShortestPathOrder = []
+    let current = finishNode
+    while(current !== null){
+        nodesInShortestPathOrder.push(current)
+        current = current.previousNode
     }
+
+    return nodesInShortestPathOrder
 }
